c27b62be6a52a409a4b14b20d6bc0091e4d01ca2
Jiewen Ying <37558033+Cloudke@users.noreply.github.com>
2021-01-05 16:57:28 -0500

    edit3

--- src/Path.java
+++
@@ -1,7 +0,0 @@
-
-public class Path {
-	String databaseName = "JiewenYing";
-	String url = "jdbc:mysql://jiewenyingmysql.c2hzoc0o1r4x.us-east-1.rds.amazonaws.com:3306/";
-	String username = "JiewenYing";
-	String password = "12fdzbhy";
-}
